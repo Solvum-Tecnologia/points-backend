6fd9ccd6cbacbe425ffe394a39ac6bd1680c56ae
Ramon Valeriano <rrvaleriano@gmail.com>
2024-12-12 22:06:03 -0300

    Criando Prefixo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import usuarioRoutes from "./usuarioRoutes.js"; // Importar as rotas de usuário
 
+const prefixProjeto = "/points/v1";
+
 const routers = (app) => {
-    app.route("/").get((req, res) => {
-      res.status(200).send({ message: "OK" });
-    });
-  
-    app.use(express.json(), usuarioRoutes);
+  app.route("/").get((req, res) => {
+    res.status(200).send({ message: "OK" });
+  });
+
+  app.use(prefixProjeto, express.json(), usuarioRoutes);
 };
-  
+
 export default routers;
